9f1aaea90a24c5b87984319fbd48c07e62b2bc10
Artur Gomes <artur@joinlets.de>
2023-05-16 16:35:43 -0400

    rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import CadastroMedicos from './components/CadastroMedicos';
 import Agendamento from './components/Agendamento';
 import ListagemAgendamentos from './components/ListagemAgendamentos';
-import { ConsultorioProvider } from './context/ConsultorioContext';
 
 function App() {
   return (
-    <ConsultorioProvider>
-      <Router>
-        <Routes>
-          <Route path="/cadastro-medicos" element={<CadastroMedicos />} />
-          <Route path="/agendamento" element={<Agendamento />} />
-          <Route path="/listagem-agendamentos" element={<ListagemAgendamentos />} />
-          <Route path="*" element={<Navigate to="/agendamento" replace />} />
+    <Router>
+      <Routes>
+        <Route path="/cadastro-medicos" element={<CadastroMedicos />} />
+        <Route path="/agendamento" element={<Agendamento />} />
+        <Route path="/listagem-agendamentos" element={<ListagemAgendamentos />} />
+        <Route path="*" element={<Navigate to="/agendamento" replace />} />
 
 
-        </Routes>
-      </Router>
-    </ConsultorioProvider>
+      </Routes>
+    </Router>
 
   );
 }
--- src/components/ListagemAgendamentos/index.tsx
+++ src/components/ListagemAgendamentos/index.tsx
@@ -1,68 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { ConsultorioContext } from '../../context/ConsultorioContext';
 
-// Define os tipos para os médicos
-interface Medico {
-  nome: string;
-  email: string;
-  telefone: string;
-  crm: string;
-}
 
-// Define os tipos para os agendamentos
-interface Agendamento {
-  nomePaciente: string;
-  cpf: string;
-  telefonePaciente: string;
-  data: string;
-  medico: string;
-}
+const ListagemAgendamentos = () => {
 
-const ListagemAgendamentos: React.FC = () => {
-  const { medicos } = useContext(ConsultorioContext);
-  const { agendamentos } = useContext(ConsultorioContext);
-  const [medico, setMedico] = useState<string>('');
-  const [filtro, setFiltro] = useState<Agendamento[]>([]);
-
-  useEffect(() => {
-    if (medico) {
-      setFiltro(agendamentos.filter(a => a.medico === medico));
-    } else {
-      setFiltro(agendamentos);
-    }
-  }, [medico, agendamentos]);
 
   return (
-    <>
-      <select value={medico} onChange={e => setMedico(e.target.value)}>
-        <option value="">Todos os médicos</option>
-        {medicos.map((medico: Medico, index: number) => (
-          <option key={index} value={medico.nome}>{medico.nome}</option>
-        ))}
-      </select>
-      <table>
-        <thead>
-          <tr>
-            <th>Paciente</th>
-            <th>CPF</th>
-            <th>Telefone</th>
-            <th>Data</th>
-            <th>Médico</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filtro.map((agendamento: Agendamento, index: number) => (
-            <tr key={index}>
-              <td>{agendamento.nomePaciente}</td>
-              <td>{agendamento.cpf}</td>
-              <td>{agendamento.telefonePaciente}</td>
-              <td>{agendamento.data}</td>
-              <td>{agendamento.medico}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <p>Listar Agendamentos</p>
   );
 };
 
