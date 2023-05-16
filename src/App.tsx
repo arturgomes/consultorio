81a0b439378d0dad9b6dbf66ddf9e585b30150b9
Artur Gomes <artur@joinlets.de>
2023-05-16 16:38:21 -0400

    nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Link as RouterLink, BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 import CadastroMedicos from './components/CadastroMedicos';
 import Agendamento from './components/Agendamento';
 import ListagemAgendamentos from './components/ListagemAgendamentos';
@@ -7,6 +8,17 @@ function App() {
   return (
     <Router>
       <Routes>
+        <nav>
+          <Button component={RouterLink} to="/register" variant="contained" color="primary" style={{ marginRight: '10px' }}>
+            Registrar Médico
+          </Button>
+          <Button component={RouterLink} to="/schedule" variant="contained" color="primary" style={{ marginRight: '10px' }}>
+            Agendar Consulta
+          </Button>
+          <Button component={RouterLink} to="/view" variant="contained" color="primary">
+            Listar Consultas
+          </Button>
+        </nav>
         <Route path="/cadastro-medicos" element={<CadastroMedicos />} />
         <Route path="/agendamento" element={<Agendamento />} />
         <Route path="/listagem-agendamentos" element={<ListagemAgendamentos />} />
