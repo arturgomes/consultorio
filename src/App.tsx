7bbc148cbab8703344067419147c5bc3a43d8097
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2023-05-23 18:52:44 -0400

    conteudo da aula do dia 16 de maio

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,78 @@
 // src/App.tsx
-import { BrowserRouter, Route, Routes, Link as RouterLink } from 'react-router-dom';
-import { Button, Box } from '@mui/material';
-import { DoctorProvider } from './DoctorContext';
-import RegisterDoctor from './RegisterDoctor';
-import ScheduleAppointment from './ScheduleAppointment';
-import ViewAppointments from './ViewAppointments';
+import { BrowserRouter, Route, Routes, Link as RouterLink } from "react-router-dom";
+import CadastrarMedicos from "./components/CadastrarMedicos";
+import AgendarConsultas from "./components/AgendarConsultas";
+import ListarConsultas from "./components/ListarConsultas";
+import { Box, Button } from "@mui/material";
+import { ConsultasProvider } from "./context/ConsultasContext";
 
+/**
+ * 
+ * Nosso objetivo é criar uma aplicação com 3 funcionalidades:
+ * - cadastrar médicos 
+ *    /cadastrar
+ * - Agendar Consultas
+ *    /agendar
+ * - Listar consultas
+ *    /listar
+ */
 function App() {
+  /**
+   * A gente usa uma biblioteca do react, chamado Router DOM
+   * direciona os caminhos para os componentes
+   */
   return (
-    <DoctorProvider>
+    <ConsultasProvider>
       <BrowserRouter>
         <Box
           display="flex"
           flexDirection="column"
           alignItems="center"
           justifyContent="center"
-          style={{ minHeight: '100vh', textAlign: 'center' }}
-        >
+          style={{ minHeight: '100vh', textAlign: 'center' }}>
           <nav>
-            <Button component={RouterLink} to="/register" variant="contained" color="primary" style={{ marginRight: '10px' }}>
+            <Button
+              component={RouterLink}
+              to="/cadastrar"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: '10px' }}
+            >
               Registrar Médico
             </Button>
-            <Button component={RouterLink} to="/schedule" variant="contained" color="primary" style={{ marginRight: '10px' }}>
-              Agendar Consulta
+            <Button
+              component={RouterLink}
+              to="/agendar"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: '10px' }}
+            >
+              Agendar Consultas
             </Button>
-            <Button component={RouterLink} to="/view" variant="contained" color="primary">
+            <Button
+              component={RouterLink}
+              to="/listar"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: '10px' }}
+            >
               Listar Consultas
             </Button>
           </nav>
           <Routes>
-            <Route path="/register" element={<RegisterDoctor />} />
-            <Route path="/schedule" element={<ScheduleAppointment />} />
-            <Route path="/view" element={<ViewAppointments />} />
+            <Route path="/cadastrar" element={<CadastrarMedicos />} />
+            <Route path="/agendar" element={<AgendarConsultas />} />
+            <Route path="/listar" element={<ListarConsultas />} />
           </Routes>
         </Box>
       </BrowserRouter>
-    </DoctorProvider>
+    </ConsultasProvider>
   );
 }
 
 export default App;
+
+
+// yarn create vite consultorio --template react-ts
+
+// yarn add react-router-dom @types/react-router-dom
